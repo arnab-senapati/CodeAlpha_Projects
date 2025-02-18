1ce7b9ba28fa1e10fee96b18a41db993a39d2cce
arnab-senapati <arnabsenapati3@gmail.com>
2025-02-18 11:05:10 +0530

    Initial commit

---
+++ TASK-4[Music_Player]/js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Deewani Mastani",
+    artist: "Shreya Ghoshal",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Kudmayi",
+    artist: "Shahid Mallya",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Tabaah Ho Gaye ",
+    artist: "Shreya Ghoshal",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Ami Je Tomar 3.0",
+    artist: "Shreya Ghoshal",
+    img: "img4",
+    audio: "music4"
+  }
+]
